e7145348ea4297f5e18c9dffa7c3a9d2020e7041
Perkan <borivoje.tasovac@gmail.com>
2018-01-06 23:14:02 +0100

    Make staying logged in optional using checkbox

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
-      remember user     # Remember logged-in user automatically.
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'   # flash.now disappear as soon as there is an additional request
