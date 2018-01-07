cb734a050173c10f6f42918fd585177883264a1b
Perkan <borivoje.tasovac@gmail.com>
2018-01-07 23:41:19 +0100

    Allow showing all users
    Fill database with sample users with 'faker' gem.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]   # Before filter: arrange for a particulalr method(logged_in_user) to be called before given actions
+  before_action :logged_in_user, only: [:index, :edit, :update]   # Before filter: arrange for a particulalr method(logged_in_user) to be called before given actions
   before_action :correct_user,   only: [:edit, :update]
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
 
   def show
     @user = User.find(params[:id])
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,6 +1,17 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user       = users(:michael)
+    @other_user = users(:archer)
+  end
+
+  test "should redirect index when not logged in" do
+    get users_path
+    assert_redirected_to login_path
+  end
+
   test "should get new" do
     get signup_path
     assert_response :success
