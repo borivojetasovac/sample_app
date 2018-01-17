eaf50f2ed2fbe8c82c8b4ea30dfbf7773c924145
Borivoje <borivoje.tasovac@gmail.com>
2018-01-17 01:31:28 +0100

    Create account activation emails
    New mailer templates and updated Users create action.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,9 +18,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      flash[:success] = "Welcome to the Sample App!"    # the Rails way to display a temporary message (:success key for a message indicating a successful result)
-      redirect_to @user
+      UserMailer.account_activation(@user).deliver_now
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_path
     else
       render 'new'
     end
