5b1ffbd4f9a1a90b0ab9536586b4fef48a3abde5
Borivoje <borivoje.tasovac@gmail.com>
2018-01-17 21:31:48 +0100

    Create password resets resource

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token, :activation_token         # Creates getter and setter methods.
+  attr_accessor :remember_token, :activation_token, :reset_token         # Creates getter and setter methods.
   before_save   :downcase_email                            # This callback (method reference) is automatically called before the object is saved (created and updated).
   before_create :create_activation_digest                  # This callback is automatically called before the object is created.
   validates :name,  presence: true, length: { maximum: 50 }
@@ -48,6 +48,18 @@ class User < ApplicationRecord
   # Sends activation email.
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
+  end
+
+  # Sets the password reset attributes.
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  # Sends password reset email.
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   root   'static_pages#home'
   get    '/help',    to: 'static_pages#help'
   get    '/about',   to: 'static_pages#about'
@@ -10,4 +14,5 @@ Rails.application.routes.draw do
   delete '/logout',  to: 'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]   # create named route for edit action (GET is mapped to edit action now)
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 end
