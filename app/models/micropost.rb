d34b59a0dec1e778c8cfb55780fa7c6eae397227
Borivoje <borivoje.tasovac@gmail.com>
2018-01-18 18:23:44 +0100

    Add reverse ordering to user.microposts method

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,6 @@
 class Micropost < ApplicationRecord
   belongs_to :user
+  default_scope -> { order(created_at: :desc) }   # Order from newest to oldest.
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
 end
