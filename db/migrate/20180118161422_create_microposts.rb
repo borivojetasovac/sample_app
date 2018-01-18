026cf4a0b37780a40c23a71e4125281d42f61fec
Borivoje <borivoje.tasovac@gmail.com>
2018-01-18 17:28:03 +0100

    Add basic micropost model

---
+++ db/migrate/20180118161422_create_microposts.rb
@@ -0,0 +1,11 @@
+class CreateMicroposts < ActiveRecord::Migration[5.1]
+  def change
+    create_table :microposts do |t|
+      t.text :content
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+    add_index :microposts, [:user_id, :created_at]    # multiple key index - to retrieve all the microposts of a given user
+  end
+end
