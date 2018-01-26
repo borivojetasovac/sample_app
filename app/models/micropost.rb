9ef41d28ee53a77d6969546be5b0f54a2a111150
Borivoje <borivoje.tasovac@gmail.com>
2018-01-26 14:06:25 +0100

    Basic image upload

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,7 @@
 class Micropost < ApplicationRecord
   belongs_to :user
   default_scope -> { order(created_at: :desc) }   # Order from newest to oldest.
+  mount_uploader :picture, PictureUploader        # CarrierWave associate the uploaded image with a model (attribute and the class name fo the uploader)
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
 end
