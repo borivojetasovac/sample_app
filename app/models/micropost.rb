c0e2e8a35f6eacc599c3a60c9444ca84351099c8
Borivoje <borivoje.tasovac@gmail.com>
2018-01-26 14:25:51 +0100

    Add image validation

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -4,4 +4,14 @@ class Micropost < ApplicationRecord
   mount_uploader :picture, PictureUploader        # CarrierWave associate the uploaded image with a model (attribute and the class name fo the uploader)
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+  validate  :picture_size   # validate (as opposed to validates) - custom validation
+
+  private
+
+    # Validates the size of an uploaded picture.
+    def picture_size
+      if picture.size > 5.megabytes
+        errors.add(:picture, "should be less than 5MB")
+      end
+    end
 end
